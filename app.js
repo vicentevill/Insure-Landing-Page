9ebbdad2077f2fbd235c1b0a5e1e9adfe1a464e7
thekingvice <villarrealvicente92@gmail.com>
2023-02-27 15:24:00 -0800

    commit

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+const hamburgerMenu = document.querySelector("img.hamburger-menu");
+const main = document.querySelector("main");
+const mobileNav = document.querySelector("nav.mobile-nav");
+let hamburgerMenuState = 0;
 
+hamburgerMenu.addEventListener("click", () => {
+  if (hamburgerMenuState === 0) {
+    hamburgerMenuState = 1;
+    main.style.display = "none";
+    mobileNav.style.display = "flex";
+  } else {
+    hamburgerMenuState = 0;
+    main.style.display = "";
+    mobileNav.style.display = "";
+  }
+});
